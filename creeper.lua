96e697d1d9f7bc8bc80a6caaa679013f670c608d
João Vitor <64005648+JaoVitorMS@users.noreply.github.com>
2024-05-06 21:25:53 -0300

    Really first commit

---
+++ creeper.lua
@@ -0,0 +1,21 @@
+local MONSTER_NAME = "Creeper"
+local description = "Um monstro que explode"
+local mobSound = "Tsssssssss"
+local health = 100
+local isAngry = true
+local damage = 50
+local speed = 10
+local jumpHeight = 2
+
+
+
+print("------------------------")
+print("| " .. MONSTER_NAME)
+print("| " .. description)
+print("| " .. mobSound)
+print("| " .. health)
+print("| " .. tostring(isAngry))
+print("| " .. damage)
+print("| " .. speed)
+print("| " .. jumpHeight)
+print("------------------------")
